be4b706ea9e3faf633af1ff0aaa66a4773857d8e
Thaniyell <essien.nathan@yahoo.com>
2022-11-30 16:19:52 +0100

    Images properly displayed on technology page

--- src/Components/Pages/Technology/TechnologyItems.js
+++ src/Components/Pages/Technology/TechnologyItems.js
@@ -2,15 +2,31 @@ import React, { useState } from "react";
 import Wrapper from "../../Wrapper";
 import technologyStyles from "./technology.module.css";
 import data from "../../data.json";
+import pics0 from '../../assets/technology/image-launch-vehicle-portrait.jpg'
+import pics1 from '../../assets/technology/image-spaceport-portrait.jpg'
+import pics2 from '../../assets/technology/image-space-capsule-portrait.jpg'
+import pics00 from '../../assets/technology/image-launch-vehicle-landscape.jpg'
+import pics11 from '../../assets/technology/image-spaceport-landscape.jpg'
+import pics22 from '../../assets/technology/image-space-capsule-landscape.jpg'
 
 
+const portraitImages = [pics0, pics1, pics2]
+const landscapeImages = [pics00, pics11, pics22]
 const technology = data.technology;
 const TechnologyItems = () => {
+  const [portrait, setPortrait] = useState(pics0)
+  const [landscape, setLandscape] = useState(pics00)
   const [spaceLaunch] = useState(technology);
   const [value, setValue] = useState(0);
-
-  const { name, images, description} = spaceLaunch[value];
+  const { name, description} = spaceLaunch[value];
   //destructure the people array and pick out these properties from each index as specified in people[value]
+
+ const imageChangeHandler =(index)=>{
+  setPortrait(portraitImages[index])
+  setValue(index)
+  setLandscape(landscapeImages[index])
+ }
+
   return (
     <Wrapper>
       {technology.length > 0 && (
@@ -18,8 +34,8 @@ const TechnologyItems = () => {
           <div className={technologyStyles.left}>
             <div className={technologyStyles.image}>
               <picture>
-                <source media="(min-width: 768px)" srcSet={images.landscape} />
-              <img src={images.portrait} alt={name} width="100%" />
+                <source media="(min-width: 768px)" srcSet={portrait} />
+              <img src={landscape} alt={name} width="100%" />
               </picture>
             </div>
           </div>
@@ -36,7 +52,7 @@ const TechnologyItems = () => {
                   <div className={technologyStyles.buttons}>
                     <button
                       type="button"
-                      onClick={() => setValue(index)}
+                      onClick={() => imageChangeHandler(index)}
                       className={technologyStyles.btn}
                       key={index}
                     >
