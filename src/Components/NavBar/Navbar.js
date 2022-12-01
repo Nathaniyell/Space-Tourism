357aa3374e7e2eb68c2029aa2e39e4a8161cb806
Thaniyell <essien.nathan@yahoo.com>
2022-12-01 12:12:25 +0100

    Add Toggle functionality to navBar on small screen

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,29 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import styles from './navbar.module.css'
 import logo from '../../Components/assets/shared/logo.svg'
 import { Link } from 'react-router-dom'
+import { FaBars } from 'react-icons/fa'
+import { MdClose } from 'react-icons/md'
 
 
 
 
-const Navbar = (props
-  ) => {
+const Navbar = (props) => {
+  const [mobileNav, setMobileNav] = useState(false)
+    const toggleNav =()=>{
+    setMobileNav(prevState => !prevState)
+  
+  }
   return (
 
     <nav className={styles.navDesktop}>
       <Link to='/'><img src={logo} alt="logo" /></Link>
-      <ul>
+      <ul className={mobileNav ? styles.navMobile : styles.desktop}>
         <NavLink to='/' className={styles.navItems}>00 Home</NavLink>
         <NavLink to='/destination' className={styles.navItems}>01 Destination</NavLink>
         <NavLink to='/crew' className={styles.navItems}>02 Crew</NavLink>
         <NavLink to='/technology' className={styles.navItems}>03 Technology</NavLink>
       </ul>
+      <button type='button'className={styles.button} onClick={toggleNav} >{mobileNav ? <MdClose /> : <FaBars /> }</button>
     </nav>
-
-
-
-
 
   )
 }
